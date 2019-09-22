79e0f6e32e701470c938b71018859c50e32c43ec
Jdrenteria <jesusrenteria48@gmail.com>
2019-09-22 15:50:50 -0500

    Added if statements to javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,38 @@ var guessChoices = [];
 
 document.onkeyup = function (event) {
 
-var userGuess = event.key;
+    var userGuess = event.key;
 
- var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 
-    
+    if (options.indexOf(userGuess) > -1) {
+
+        if (userGuess === computerGuess) {
+            wins++;
+            numGuesses = 9;
+            guessChoices = [];
+        }
+
+        if (userGuess != computerGuess) {
+            numGuesses--;
+            guessChoices.push(userGuess);
+        }
+
+        if (numGuesses === 0) {
+
+            numGuesses = 9;
+            losses++;
+            guessChoices = [];
+
+
+        }
+
+     
+
+
+    }
 };
 
